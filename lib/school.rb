1c38c30262db54ceade12a5a4f0fa4cda442997e
Carter Parrett <cparrett94@hotmail.com>
2020-07-14 22:05:28 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
-# code here!
+class School
+  
+  ROSTER = {}
+    
+    def initialize(school_name, roster)
+      @school_name = school_name
+      @roster = roster
+        ROSTER << roster
+    end
+    
+    def school_name
+      @school_name
+    end
+    
+    def school_name
+      @school_name
+    end
+
+    
+    
+    
+    
+end
